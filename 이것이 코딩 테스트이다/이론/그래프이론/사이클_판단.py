f7d90894cb992781f1a2adb90f8072f661d56140
Lee Sang Won <tkddnjs206@gmail.com>
2022-05-15 00:39:23 +0900

    graph2

---
+++ 이것이 코딩 테스트이다/이론/그래프이론/사이클_판단.py
@@ -0,0 +1,41 @@
+
+def union_parent(parent, a, b):
+    a= find_parent(parent,a)
+    b=find_parent(parent,b)
+    if a<b:
+        parent[b] =a
+    else:
+        parent[a] =b
+
+# 경로 압축 기법 소스코드
+def find_parent(parent, x):
+    if parent[x] !=x:
+        parent[x] = find_parent(parent,parent[x])
+    return parent[x]
+
+# v : 노드의 개수 , e : 간선의 개수
+v, e = map(int, input().split())
+parent=[0]*(v+1)
+
+# 부모 테이블상에서,부모를 자기 자신으로 초기화
+for i in range(1,v+1):
+    parent[i] =i
+
+
+
+cycle =True
+
+for i in range(e):
+    a,b = map(int,input().split())
+    # 사이클이 발생하는 경우 종료를 한다.
+    if find_parent(parent,a) == find_parent(parent,b):
+        cycle = True
+        break
+    #사이클이 발생하지 않은 경우 합집합을 수행한다.
+    else:
+        union_parent(parent,a,b)
+
+if cycle:
+    print("cycle True")
+else:
+    print("cycle False")
---
+++ 이것이 코딩 테스트이다/이론/그래프이론/크루스칼_알고리즘.py
@@ -0,0 +1,37 @@
+
+def union_parent(parent, a, b):
+    a= find_parent(parent,a)
+    b=find_parent(parent,b)
+    if a<b:
+        parent[b] =a
+    else:
+        parent[a] =b
+
+# 경로 압축 기법 소스코드
+def find_parent(parent, x):
+    if parent[x] !=x:
+        parent[x] = find_parent(parent,parent[x])
+    return parent[x]
+
+
+# v : 노드의 개수 , e : 간선의 개수
+v, e = map(int, input().split())
+parent=[0]*(v+1) # cycle 체크를 위해서 필요할 수 밖에 없네
+
+# 모든 간선을 담을 리스트와 최종 비용을 담을 변수
+edges = []
+result = 0
+
+# 부모 테이블상에서,부모를 자기 자신으로 초기화
+for i in range(1,v+1):
+    parent[i] =i
+
+# 모든 간선에 대한 정보를 입력받기
+for _ in range(e):
+    a,b,cost = map(int, input.split())
+    edges.append((cost,a,b))
+    pass
+
+# 간선을 비용순으로 정렬하기
+edges.sort()
+
