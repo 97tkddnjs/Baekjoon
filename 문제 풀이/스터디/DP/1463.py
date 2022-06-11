bd910d44d7162374b5ccb307613d800ace6015f1
Lee Sang Won <tkddnjs206@gmail.com>
2022-06-11 17:50:46 +0900

    solve

---
+++ 문제 풀이/스터디/DP/1463.py
@@ -0,0 +1,19 @@
+
+
+
+def func(x):
+    for i in range(2,x+1):
+        dp[i] = dp[i-1]+1
+        if i%3==0:
+            dp[i] = min(dp[i], dp[i//3]+1)
+        if i%2==0:
+            dp[i] = min(dp[i], dp[i//2]+1)
+
+    return dp[x]
+
+x= int(input())
+
+dp=[0]*(x+1)
+
+
+print(func(x))
