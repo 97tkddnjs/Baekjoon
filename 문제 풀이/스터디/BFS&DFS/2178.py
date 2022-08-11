b447852389850587e1ae060da4290ec6d7e7596d
Lee Sang Won <tkddnjs206@gmail.com>
2022-08-11 23:05:53 +0900

    solve

--- 문제 풀이/스터디/BFS&DFS/2178.py
+++ 문제 풀이/스터디/BFS&DFS/2178.py
@@ -8,9 +8,37 @@ n, m = map(int, input().split())
 graph=[]
 
 for i in range(n):
-    tmp =input().split()
-    print(tmp.split(''))
-    graph.append(tmp)
+    graph.append(list(map(int, input().rstrip())))
 
 
-print(graph)
+# print(graph)
+
+queue=deque()
+queue.append((0,0))
+
+# 상하좌우
+dx = [0, 0 ,-1, 1]
+dy = [1, -1 ,0, 0]
+
+while queue:
+
+    data= queue.popleft()
+    #print(data)
+    x, y =data
+    if y==n-1 and x== m-1:
+        break
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y+ dy[i]
+        
+        if nx<0 or nx>=m or ny<0 or ny>=n:
+            continue
+        
+        if graph[ny][nx] == 1:
+            queue.append((nx,ny))
+            graph[ny][nx] = graph[y][x]+1
+            pass
+        
+
+print(graph[n-1][m-1])
