a64286fa13ea620a087e53cf363da1218c187a85
Lee Sang Won <tkddnjs206@gmail.com>
2022-09-08 00:15:50 +0900

    dp solve , dp should practice basic problem

--- 문제 풀이/스터디/다이나믹 프로그래밍/1904.py
+++ 문제 풀이/스터디/다이나믹 프로그래밍/1904.py
@@ -5,11 +5,11 @@ input = sys.stdin.readline
 N= int(input())
 
 dp=[0]*(N+1)
-print(N)
+# print(N)
 dp[1] = 1
 dp[2] = 2
 
 for i in range(3,N+1):
-    dp[i] = (dp[i-1]*2 +dp[i-2])%15746
-print(dp)
+    dp[i] = (dp[i-1] +dp[i-2])%15746
+# print(dp)
 print(dp[N])
