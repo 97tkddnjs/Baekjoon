89550d8b85d81776342fc0053dd765c95abd723b
Lee Sang Won <tkddnjs206@gmail.com>
2022-10-13 01:58:39 +0900

    solove

--- 문제 풀이/스터디/BFS&DFS/1707(이분그래프).py
+++ 문제 풀이/스터디/BFS&DFS/1707(이분그래프).py
@@ -2,37 +2,49 @@
 
 from collections import deque
 
+def bfs(start ,data):
+    queue = deque()
+    queue.append(start)
+    visited[start] = True
+    color[start] = data
+    #print(graph)
+    #print(visited)
+    #print(queue)
+    while queue and res:
+        x = queue.popleft()
+        c = color[x]
+        
+        for i in sorted(graph[x]):
+            if color[i] !=0 and c == color[i]:
+                return False
+                        
+            if not visited[i]:
+                visited[i]= True
+                queue.append(i)
+                color[i] = -c
+    return True
 
 test = int(input())
 
 for _ in range(test):
     v, e = map(int, input().split())
-
+    res=True
     visited = [False]*(v+1)
-    
     color = [0]*(v+1)
-    print(color)
     graph= [[] for _ in range(v+1)]
-    print(graph)
+    
     for i in range(e):
         a,b=map(int, input().split())
         graph[a].append(b)
+        graph[b].append(a)
     
-    queue = deque()
-    queue.append([1])
-    visited[1] = True
-    color[1] = 1
-    while queue:
-        x = queue.popleft()
-        c = color[x]
-
-        for i in graph[x]:
-            if color[i] !=0 and c == color[i]:
-                print("NO")
-                break                
-            if not visited[i]:
-                visited[i]= True
-                queue.append(i)
-                color[i] = -c  
-
-    print("YES")
+    
+    # 이걸로 완전 연결 그래프인지 체크했다...
+    for i in range(1,v+1):
+        if not visited[i]:
+            res =  bfs(i,1)
+            if not res:
+                break
+    print('YES' if res else 'NO')
+    
+    
