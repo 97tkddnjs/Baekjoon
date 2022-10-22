9879b93fc7ed1926e287ffbc515c3279a73a87bf
Lee Sang Won <tkddnjs206@gmail.com>
2022-10-22 23:58:25 +0900

    solve

--- 문제 풀이/스터디/BFS&DFS/5014(스타트링크).py
+++ 문제 풀이/스터디/BFS&DFS/5014(스타트링크).py
@@ -1,3 +1,4 @@
+from collections import deque
 import sys
 input =sys.stdin.readline
 
@@ -5,5 +6,41 @@ input =sys.stdin.readline
 # input
 # F S G U D
 
+
 F, S, G, U, D = map(int,input().split())
 
+#visited=[False]
+
+
+
+way =[U,-D]
+print(way)
+graph =[0]*(F+1)
+
+
+def bfs(start):
+    queue = deque()
+    
+    queue.append(start)
+
+    while queue:
+        
+
+        pos =queue.popleft()
+        # print("pos ",pos)
+        if graph[G] !=0:
+            return
+
+        for i in range(2):
+            npos = pos+ way[i]
+            # print("npos ",npos)
+            if npos<1 or npos > F:
+                continue
+
+            graph[npos] = graph[pos]+1
+            print(graph)
+            queue.append(npos)
+
+
+print(bfs(S))
+print(graph) 
