18e63cee428b28cde10f8c8c6e09f9249817fcb4
Lee Sang Won <tkddnjs206@gmail.com>
2022-10-25 23:51:37 +0900

    solve

--- 문제 풀이/스터디/BFS&DFS/9934(완전 이진 트리).py
+++ 문제 풀이/스터디/BFS&DFS/9934(완전 이진 트리).py
@@ -1,5 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-def tree():
+def tree(tree_data):
+
+    print()
+    tree()
+
     pass
+
+n= int(input())
+
+data = list(map(int,input().split()))
+
+length = len(data)
+
+
+
