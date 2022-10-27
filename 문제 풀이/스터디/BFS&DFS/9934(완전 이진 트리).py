27f84002a7b9c51f7cc74863faace669eb330fea
Lee Sang Won <tkddnjs206@gmail.com>
2022-10-27 21:49:53 +0900

    tree

--- 문제 풀이/스터디/BFS&DFS/9934(완전 이진 트리).py
+++ 문제 풀이/스터디/BFS&DFS/9934(완전 이진 트리).py
@@ -1,18 +1,22 @@
 import sys
 input = sys.stdin.readline
 
+# 답지 참조할 것!
 def tree(tree_data):
+    length = len(tree_data)
 
-    print()
-    tree()
-
+    try:    
+        print(tree_data[length//2])
+    except:
+        return
+    tree(tree_data[:length//2])
+    tree(tree_data[length//2:])
     pass
 
 n= int(input())
 
 data = list(map(int,input().split()))
 
-length = len(data)
-
+tree(data)
 
 
