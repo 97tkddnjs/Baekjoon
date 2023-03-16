6d86c2d284dd8031f6e5abff835b3b39174f8e25
Lee Sang Won <tkddnjs206@gmail.com>
2023-03-17 00:52:04 +0900

    ...

--- 문제 풀이/삼성sw/14503(로봇청소기).py
+++ 문제 풀이/삼성sw/14503(로봇청소기).py
@@ -15,16 +15,41 @@ dir = deque([0,1,2,3])
 dx=[0 ,-1, 0, 1 ] #  c
 dy=[-1, 0, 1, 0]  # r
 
-def algo(x,y, look):
+def algo(x,y , stop):
     
+    check = False
     
+
+    # 주변 4칸 체크
     for i in range(4):
         
         if  y < 0 or y >= n or x<0 or x >= m:
             continue
-        
-        if data[y][x] == 0:
-            pass
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if data[ny][nx] == 0:
+            check =True
+            break
+    print(check)
+    if check:
+        dir.rotate(1)
+        nx = x+dx[dir[0]]
+        ny = y + dy[dir[0]]
+        if data[ny][nx] == 0:
+            x = nx
+            y = ny
+    else:
+        dir.rotate(2)
+        nx = x+dx[dir[0]]
+        ny = y + dy[dir[0]]
+        if data[ny][nx] == 0:
+            x = nx
+            y = ny
+        else:
+            stop =True
+        dir.rotate(2) # 원복
+    
+    return ( x, y , stop)
 
 
 r, c ,d = map(int, input().split())
@@ -36,5 +61,21 @@ for _ in range(n):
 
 
 print(data, dir)
+stop =False
+x = c
+y = r
+cnt = 0
+s=set()
+while not stop:
+    x, y, stop = algo(x, y , stop)
+    print(x,y, stop)
+    s.add((y,x))
+    cnt+=1
+    if cnt >=100:
+        break
+
+print(s)
+print(cnt)
+
 
 
