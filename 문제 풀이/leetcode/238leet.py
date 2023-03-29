a2f12ec614dd46e85fb44b83e9530418025514fc
97tkddnjs <tkddnjs206@gmail.com>
2023-03-29 17:39:18 +0900

    Update 238leet.py
    solve code

--- 문제 풀이/leetcode/238leet.py
+++ 문제 풀이/leetcode/238leet.py
@@ -15,3 +15,29 @@ class Solution(object):
         # case 0에선 안됨 
         # 닫시 생각...
         return output
+    
+    
+# 맞춘 코드    
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        length = len(nums)-1
+
+        left = []
+        right = []
+        print(nums)
+        for i, data in enumerate(nums):
+            # check
+            left_idx = i -1
+            right_idx = length -i +1
+
+            if  left_idx < 0 or right_idx >= len(nums):
+                left.append(1)
+                right.append(1)
+            else:
+                left.append(left[i-1]*nums[left_idx])
+                right.append(right[i-1]*nums[right_idx])
+            
+        outputs =[ left[i]*right[length-i] for i in range(len(nums))]    
+
+        return outputs
